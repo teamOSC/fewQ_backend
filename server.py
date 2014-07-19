e47b9c4f250107946fff2206c6d7222665ac3779
sauravtom <stomatrix@gmail.com>
2014-07-19 18:55:43 +0530

    added new endpoint

--- server.py
+++ server.py
@@ -40,16 +40,57 @@ def index():
         name = request.args.get('name')
         email = request.args.get('email')
         phone = request.args.get('phone')
-
+        profile = request.args.get('profile') or ''
     else:
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-    
+        profile = request.args.get('profile') or ''
+
     D = DB()
-    D.add_to_db(name,email,phone)
+    D.add_to_db(name,email,phone,profile)
     
     return "200"
+
+@app.route('/customer_in')
+def customer_in():
+    user_id = request.args.get('user_id')
+    store_id = request.args.get('store_id')
+    d = {}
+
+    d['type'] = 'mall'
+    d['items'] = []
+    d2 = {}
+    d2['title'] = 'Pantaloons'
+    d2['text'] = '50%% discount on full sleeves shirts'
+    d2['image'] = 'https://joefresh-resource-dev.joefresh.com/JF/b3/WS14WT2290/Dark%20Pink/T.jpg'
+    d['items'].append(d2)
+    d2 = {}
+    d2['title'] = 'Isle 3'
+    d2['text'] = '40%% discount on Cheif songs'
+    d2['image'] = 'https://joefresh-resource-dev.joefresh.com/JF/b3/KGF4FW9484/Pink/T.jpg'
+    d['items'].append(d2)
+    d2 = {}
+    d2['title'] = 'Books'
+    d2['text'] = '20%% discount on Books'
+    d2['image'] = 'http://www.llsoft.ca/products/images/BookManager256x256.png'
+    d['items'].append(d2)
+    d2 = {}
+    d2['title'] = 'tops'
+    d2['text'] = '10%% discount on tops'
+    d2['image'] = 'http://www.wakeeffects.com/shop/images/product/d/dilemma-tank-top-r6679-256px-256px.png'
+    d['items'].append(d2)
+
+    return json.dumps(d)
+
+@app.route('/customer_out')
+def customer_out():
+    user_id = request.args.get('user_id')
+    store_id = request.args.get('store_id')
+    return ""
+
+def get_user_json(user_id,store_id):
+    return ""
 
 def test():
     D =DB()
